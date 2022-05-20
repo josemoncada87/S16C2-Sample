ccd9d9d460618dd18084edf897da19271705bd96
josemoncada87 <josemoncada87@hotmail.com>
2022-05-20 13:43:16 -0500

    build for gh pages

--- src/player.test.ts
+++ src/player.test.ts
@@ -1,38 +1,9 @@
 import {
   expect, test,
 } from 'vitest';
-import Player from './player';
 
 // Edit an assertion and save to see HMR in action
 
-test('testing right', () => {
-  // const player = new Player(200, 200);
-  // player.move(0);
-  // expect(player.getX()).toBe(201);
+test('testing sample', () => {
+  expect(true).toBe(true);
 });
-
-test('testing left', () => {
-  // const player = new Player(200, 200);
-  // player.move(1);
-  // expect(player.getX()).toBe(199);
-});
-
-/*
-test('Math.sqrt()', () => {
-  expect(Math.sqrt(4)).toBe(2);
-  expect(Math.sqrt(144)).toBe(12);
-  expect(Math.sqrt(2)).toBe(Math.SQRT2);
-});
-
-test('JSON', () => {
-  const input = {
-    foo: 'hello',
-    bar: 'world',
-  };
-
-  const output = JSON.stringify(input);
-
-  expect(output).eq('{"foo":"hello","bar":"world"}');
-  assert.deepEqual(JSON.parse(output), input, 'matches original');
-});
-*/
